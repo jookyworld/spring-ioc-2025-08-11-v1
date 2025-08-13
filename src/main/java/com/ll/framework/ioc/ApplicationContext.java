f3c18fcc22d2448057d99e374416e3bc88c0779f
jooky <jooky0327@naver.com>
2025-08-13 23:09:50 +0900

    t2

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,11 +1,17 @@
 package com.ll.framework.ioc;
 
+import com.ll.domain.testPost.testPost.repository.TestPostRepository;
+import com.ll.domain.testPost.testPost.service.TestPostService;
+
 public class ApplicationContext {
     public ApplicationContext() {
 
     }
 
     public <T> T genBean(String beanName) {
+        if (beanName.equals("testPostService")) {
+            return (T) new TestPostService(new TestPostRepository());
+        }
         return (T) null;
     }
 }
