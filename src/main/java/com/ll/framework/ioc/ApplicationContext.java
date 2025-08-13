72162d988631e0961fa6e93e11f9cec5e8214da4
jooky <jooky0327@naver.com>
2025-08-13 23:19:31 +0900

    t6

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,15 +1,18 @@
 package com.ll.framework.ioc;
 
 import com.ll.domain.testPost.testPost.repository.TestPostRepository;
+import com.ll.domain.testPost.testPost.service.TestFacadePostService;
 import com.ll.domain.testPost.testPost.service.TestPostService;
 
 public class ApplicationContext {
     private static TestPostService testPostService;
     private static TestPostRepository testPostRepository;
+    private static TestFacadePostService testFacadePostService;
 
     public ApplicationContext() {
         testPostRepository = new TestPostRepository();
         testPostService = new TestPostService(testPostRepository);
+        testFacadePostService = new TestFacadePostService(testPostService, testPostRepository);
     }
 
     public <T> T genBean(String beanName) {
@@ -17,6 +20,8 @@ public class ApplicationContext {
             return (T) testPostService;
         } else if (beanName.equals("testPostRepository")) {
             return (T) testPostRepository;
+        } else if (beanName.equals("testFacadePostService")) {
+            return (T) testFacadePostService;
         }
         return (T) null;
     }
